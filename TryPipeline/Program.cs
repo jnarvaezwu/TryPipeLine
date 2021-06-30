372e0bb01897e80f0e961677ff15846f315e50d2
Jorge <jormelcn@gmail.com>
2021-06-30 09:24:18 -0500

    First Commit

---
+++ TryPipeline/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryPipeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
